973a4469be38d91ad9d20ca3bc80ec32cee3522a
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-11-25 13:56:48 -0700

    Models back to sqlite for dev purposes

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ from flask_login import UserMixin
 from playhouse.db_url import connect
 
 
-DATABASE = connect(os.environ.get('DATABASE_URL'))
+# DATABASE = connect(os.environ.get('DATABASE_URL'))
+DATABASE = SqliteDatabase('eat.sqlite')
 
 class User(UserMixin, Model):
 	email = CharField(unique=True)
